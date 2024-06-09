7b9e0b0990b322e2665bdac899b87971ac0505f0
smonsays <smonsays@users.noreply.github.com>
2024-06-09 11:18:37 -0400

    add target network blocks that perform approriate variance scaling for stable gradient-based training

--- hypernetwork.py
+++ hypernetwork.py
@@ -11,11 +11,16 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-from typing import Tuple
+from functools import partial
+from typing import Any, Callable, Tuple
 
+import chex
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
+from flax.linen.dtypes import promote_dtype
+from flax.linen.module import compact
+from jax import lax
 from jax.flatten_util import ravel_pytree
 
 
@@ -26,14 +31,10 @@ class Hypernetwork(nn.Module):
     input_shape: Tuple[int]
     target_network: nn.Module
     weight_generator: nn.Module
-    rng_keys: Tuple[str] = ()
+    rng_collection_names: Tuple[str] = ()
 
-    @staticmethod
-    def get_target_apply(
-        module: nn.Module, input_shape: Tuple[int], rng_keys: Tuple[str]
-    ):
-
-        init_rngs = {key: jax.random.key(0) for key in ("params",) + rng_keys}
+    def get_target_apply(self, module: nn.Module, input_shape: Tuple[int]):
+        init_rngs = {key: jax.random.key(0) for key in ("params",) + self.rng_collection_names}
         variables = module.init(init_rngs, jnp.empty(input_shape))
 
         if len(variables) != 1:
@@ -44,8 +45,14 @@ class Hypernetwork(nn.Module):
         params_flat, unflatten_fn = ravel_pytree(variables["params"])
         num_params = len(params_flat)
 
-        def apply_fn(params_flat, input, rngs):
+        @partial(jnp.vectorize, signature="(n),(s,d),()->(s,r)")
+        def apply_fn(params_flat, input, rng):
             variables = {"params": unflatten_fn(params_flat)}
+            rngs = dict(zip(
+                self.rng_collection_names,
+                jax.random.split(rng, len(self.rng_collection_names))
+            ))
+
             return module.apply(variables, input, rngs=rngs)
 
         return apply_fn, num_params
@@ -54,13 +61,110 @@ class Hypernetwork(nn.Module):
         self.apply_fn, self.num_params = self.get_target_apply(
             module=self.target_network,
             input_shape=self.input_shape,
-            rng_keys=self.rng_keys,
         )
         # NOTE: Assuming weight generator output_dim defined as attribute `features`
         self.weight_generator_ = self.weight_generator(features=self.num_params)
 
     def __call__(self, latent, input):
         params_flat = self.weight_generator_(latent)
-        rngs = {key: self.make_rng(key) for key in self.rng_keys}
+        rng = self.make_rng("target")  # give target network separate rng stream
 
-        return self.apply_fn(params_flat, input, rngs=rngs)
+        return self.apply_fn(params_flat, input, rng)
+
+
+class VarianceScaledKernel(nn.Module):
+    """
+    Kernel with variance scaling (e.g. glorot, lecun) during forward instead of at initialization.
+    """
+    input_dim: int
+    output_dim: int
+    distribution: str = "truncated_normal"
+    scale: float = 1.0
+    mode: str = "fan_in"
+    param_dtype: Any = jnp.float32
+
+    def setup(self) -> None:
+        # Compute normalizer to be applied during forward pass instead of using it for init
+        if self.mode == "fan_in":
+            denominator = self.input_dim
+        elif self.mode == "fan_out":
+            denominator = self.output_dim
+        elif self.mode == "fan_avg":
+            denominator = (self.input_dim + self.output_dim) / 2
+        else:
+            raise ValueError(f"invalid mode for variance scaling initializer: {self.mode}")
+        variance = jnp.array(self.scale / denominator, dtype=self.param_dtype)
+
+        # Initialize unnormalized kernel
+        assert jnp.issubdtype(self.param_dtype, jnp.floating), "No support for complex numbers."
+
+        if self.distribution == "truncated_normal":
+            def init_fn(key, shape, dtype):
+                return jax.random.truncated_normal(key, -2, 2, shape, dtype)
+
+            # constant is stddev of standard normal truncated to (-2, 2)
+            self.stddev = jnp.sqrt(variance) / jnp.array(0.87962566103423978, self.param_dtype)
+        elif self.distribution == "normal":
+            init_fn = jax.random.normal
+            self.stddev = jnp.sqrt(variance)
+        elif self.distribution == "uniform":
+            init_fn = partial(jax.random.uniform, minval=-1)
+            self.stddev = jnp.sqrt(3 * variance)
+        else:
+            raise ValueError(f"invalid mode for variance scaling initializer: {self.mode}")
+
+        self.kernel = self.param(
+            "unnormalized_kernel",
+            init_fn,
+            (self.input_dim, self.output_dim),
+            self.param_dtype,
+        )
+
+    def __call__(self) -> chex.Array:
+        # Normalize kernel
+        return self.kernel * self.stddev
+
+
+class VarianceScaledDense(nn.Dense):
+    @compact
+    def __call__(self, inputs: chex.Array) -> chex.Array:
+        kernel = VarianceScaledKernel(
+            input_dim=inputs.shape[-1],
+            output_dim=self.features,
+            param_dtype=self.param_dtype
+        )()
+        if self.use_bias:
+            bias = self.param(
+                "bias", self.bias_init, (self.features,), self.param_dtype
+            )
+        else:
+            bias = None
+        inputs, kernel, bias = promote_dtype(inputs, kernel, bias, dtype=self.dtype)
+
+        y = lax.dot_general(
+            inputs,
+            kernel,
+            (((inputs.ndim - 1,), (0,)), ((), ())),
+            precision=self.precision,
+        )
+        if bias is not None:
+            y += jnp.reshape(bias, (1,) * (y.ndim - 1) + (-1,))
+        return y
+
+
+class VarianceScaledMLP(nn.Module):
+    output_dim: int
+    hidden_dim: int
+    num_hidden: int
+    nonlinearity: Callable = nn.relu
+
+    @compact
+    def __call__(self, inputs: chex.Array) -> chex.Array:
+        x = inputs
+        for i in range(self.num_hidden):
+            x = VarianceScaledDense(self.hidden_dim, name=f"norm_dense{i}")(x)
+            x = self.nonlinearity(x)
+
+        x = VarianceScaledDense(self.output_dim, name="norm_dense_out")(x)
+
+        return x
--- test_hypernetwork.py
+++ test_hypernetwork.py
@@ -12,11 +12,12 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 """
 import unittest
 
+import chex
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
 
-from hypernetwork import Hypernetwork
+from hypernetwork import Hypernetwork, VarianceScaledDense, VarianceScaledKernel, VarianceScaledMLP
 
 
 class HypernetworkTest(unittest.TestCase):
@@ -25,22 +26,66 @@ class HypernetworkTest(unittest.TestCase):
         input_dim = 7
         output_dim = 5
         batch_dim = 13
+        meta_batch_dim = 17
 
-        rng = jax.random.PRNGKey(0)
+        rng = jax.random.key(0)
         input = jnp.ones((batch_dim, input_dim))
-        latent = jnp.ones((latent_dim))
+        latent = jnp.ones((meta_batch_dim, latent_dim))
 
         hnet = Hypernetwork(
             input_shape=input.shape,
-            target_network=nn.Dense(output_dim),
-            rng_keys=("dropout", ),
+            target_network=VarianceScaledMLP(output_dim, 3, 32),
+            rng_collection_names=("dropout", ),
             weight_generator=nn.Dense
         )
-        init_rngs = {'params': rng, 'dropout': rng}
-        variables = hnet.init(init_rngs, latent, input)
-        output = hnet.apply(variables, latent, input, rngs={'dropout': rng})
+        init_rngs = {'params': rng, 'dropout': rng, 'target': rng}
+        variables = jax.jit(hnet.init)(init_rngs, latent, input)
+        output = jax.jit(hnet.apply)(variables, latent, input, rngs={'dropout': rng, 'target': rng})
+        chex.assert_shape(output, (meta_batch_dim, batch_dim, output_dim))
 
-        assert output.shape == (batch_dim, output_dim)
+    def test_variance_scaled_kernel(self):
+        rng = jax.random.PRNGKey(0)
+        input_dim = 5
+        output_dim = 7
+        batch_dim = 11
+        input = jnp.ones((batch_dim, input_dim))
+
+        kernel_fn = VarianceScaledKernel(input_dim, output_dim, distribution="truncated_normal")
+        variables = kernel_fn.init({"params": rng})
+        kernel = kernel_fn.apply(variables)
+        chex.assert_shape(kernel, (input_dim, output_dim))
+
+        dense_fn = nn.Dense(output_dim)
+
+        @jax.vmap
+        def init_both(rng):
+            rng1, rng2 = jax.random.split(rng)
+
+            kernel_fn = VarianceScaledKernel(input_dim, output_dim, distribution="truncated_normal")
+            vars = kernel_fn.init({"params": rng1})
+            kernel1 = kernel_fn.apply(vars)
+
+            vars = dense_fn.init({"params": rng2}, input)
+            kernel2 = vars["params"]["kernel"]
+
+            return kernel1, kernel2
+
+        rngs = jax.random.split(rng, 20480)
+        k1, k2 = init_both(rngs)
+
+        chex.assert_trees_all_close(jnp.std(k1), jnp.std(k2), atol=1e-3)
+
+    def test_variance_scaled_dense(self):
+        rng = jax.random.PRNGKey(0)
+        input_dim = 7
+        output_dim = 5
+        batch_dim = 3
+        input = jnp.ones((batch_dim, input_dim))
+
+        dense_fn = VarianceScaledDense(output_dim)
+        variables = dense_fn.init({"params": rng}, input)
+        output = dense_fn.apply(variables, input)
+        chex.assert_shape(output, (batch_dim, output_dim))
 
 
 if __name__ == '__main__':
